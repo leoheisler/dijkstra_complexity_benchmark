7afca81b94a5ecf99f817ddf3f9037c4f4589a90
leoheisler <leoheisler@hotmail.com>
2025-03-23 21:14:15 -0300

    debug lines

--- run_graphs.sh
+++ run_graphs.sh
@@ -5,5 +5,7 @@ dir="./graphs"
 
 # Itera sobre cada arquivo encontrado na pasta (recursivamente)
 while IFS= read -r graph_file; do
+    echo $graph_file
     ./build/dijk "?" "?" < $graph_file
+    echo "-----------------------------"
 done < <(find "$dir" -type f)
