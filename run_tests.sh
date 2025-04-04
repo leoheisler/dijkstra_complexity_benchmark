6aa4077e2e7ab413bcaf7ad2e3a2a93462937ca9
leoheisler <leoheisler@hotmail.com>
2025-04-03 22:37:26 -0300

    chng

--- run_tests.sh
+++ run_tests.sh
@@ -7,7 +7,7 @@ for k_value in {2,4,8,16}; do
     outfile_name="results/k-results/k_${k_value}.csv"
 
     echo "testing k value - ${k_value}:"
-    echo "FILE_NAME,VERTEX_NUM,EDGES_NUM,INSERTS,UPDATES,DELETES,R_AVG,DURATION,RESIDENT_MEM" > "$outfile_name"
+    echo "FILE_NAME,K_NUM,VERTEX_NUM,EDGES_NUM,INSERTS,UPDATES,DELETES,R_AVG,DURATION,RESIDENT_MEM" > "$outfile_name"
     
     while IFS= read -r graph_file; do
         echo "   test for ${graph_file}"
@@ -31,7 +31,7 @@ done
 echo "testing var in edges"
 dir="./graphs/change_edges"
 outfile_name="results/var_edges.csv"
-echo "FILE_NAME,VERTEX_NUM,EDGES_NUM,INSERTS,UPDATES,DELETES,R_AVG,DURATION,RESIDENT_MEM" > "$outfile_name"
+echo "FILE_NAME,K_NUM,VERTEX_NUM,EDGES_NUM,INSERTS,UPDATES,DELETES,R_AVG,DURATION,RESIDENT_MEM" > "$outfile_name"
 
 while IFS= read -r graph_file; do
     echo "   test for ${graph_file}"
@@ -54,7 +54,7 @@ done < <(find "$dir" -type f)
 echo "testing var in edges"
 dir="./graphs/change_vertices"
 outfile_name="results/var_vertex.csv"
-echo "FILE_NAME,VERTEX_NUM,EDGES_NUM,INSERTS,UPDATES,DELETES,R_AVG,DURATION,RESIDENT_MEM" > "$outfile_name"
+echo "FILE_NAME,K_NUM,VERTEX_NUM,EDGES_NUM,INSERTS,UPDATES,DELETES,R_AVG,DURATION,RESIDENT_MEM" > "$outfile_name"
 
 while IFS= read -r graph_file; do
     echo "   test for ${graph_file}"
@@ -78,7 +78,7 @@ done < <(find "$dir" -type f)
 echo "testing dijkstra"
 graph_file="./graphs/dimacs_graphs/USA-road-d.NY.gr"
 outfile_name="results/NY_stats.csv"
-echo "FILE_NAME,VERTEX_NUM,EDGES_NUM,INSERTS,UPDATES,DELETES,R_AVG,DURATION,RESIDENT_MEM" > "$outfile_name"
+echo "FILE_NAME,VERTEX_NUM,K_NUM,EDGES_NUM,INSERTS,UPDATES,DELETES,R_AVG,DURATION,RESIDENT_MEM" > "$outfile_name"
 
 for i in {1..30}; do 
     echo "   test for ${i}"
